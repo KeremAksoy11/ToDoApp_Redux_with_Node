be89a78eee9793896e2b0ee3899451a7b06276b7
Kerem AKSOY <keremaksoy1127@gmail.com>
2022-07-18 19:08:09 +0300

    I added API Post request.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,43 @@
 import {useState} from 'react';
 import { useDispatch} from 'react-redux'
-
-import { addToDo } from '../redux/todos/todosSlice';
-
+import { addToDoAsync } from '../redux/todos/todosSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
 	const [title, setTitle] = useState('');
 	
 
 	const dispatch = useDispatch();
+	const İsLoading = useSelector((state)=> state.todos.addNewTodoLoading)
+	const Error = useSelector((state)=> state.todos.addNewTodoError);
 
-	const handleSubmit = (e) =>{ // bir istek gönderdiğimizde sayfayı yönlendirmemek için kullanıyoruz. Handlesubmit'in amacı budur.
-	if (!title) return; // eğer title içi boşşa direkt dön dedik ve veri girişini engelledik.
+	const handleSubmit = async (e) =>{ 
+	// bir istek gönderdiğimizde sayfayı yönlendirmemek için kullanıyoruz. Handlesubmit'in amacı budur.
+	if (!title) return; 
+	// eğer title içi boşşa direkt dön dedik ve veri girişini engelledik.
 	e.preventDefault();
-	dispatch(addToDo({ title}))
+	await dispatch(addToDoAsync({ title}))
 	setTitle('');
+	}
+
+	if(Error)
+	{
+		alert(Error);
+		return
 	}
 
     return ( 
     <div>
         <header className="header">
 		<h1>todos</h1>
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit} style={{display: 'flex', alignItems: 'center'}}>
 			<input className="new-todo"
+			disabled={İsLoading}
 			placeholder="What needs to be done?"
 			autoFocus 
 			value={title} 
 			onChange={(e) => setTitle(e.target.value)}/>
+			{İsLoading && <span style={{paddingRight: 10}}>Loading...</span>}
 		</form>
 	</header>
     </div>);
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -1,74 +1,82 @@
-import {createSlice} from '@reduxjs/toolkit'
-import { nanoid } from 'nanoid'
-import {createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const getTodosAsync = createAsyncThunk('todos/getTodosAsync', async () => {
-    const res = await fetch('http://localhost:7000/todos');
-    return await res.json();
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`);
+    return res.data;
 });
 
 
-export const todosSlice = createSlice({
-    name : 'todos',
-    initialState : {
-        items : [],
-        İsLoading : false,
-        error : null,
-        activeFilter : 'all',
-    },
-    reducers :{
-        addToDo : {
-        reducer :(state,action) =>{
-            state.items.push(action.payload)
-        },
-        prepare : ({title}) => { // Sürekli aynı verileri statik olarak yazacaksak ve reducerı çok yerde kullanacaksak, koddan kar etmek için reducer'a gelen action.payloadını burada manipüle ederek kısa kod satırlarıyla bitirebiliriz işi.
-            return {
-                payload : {
-                    id : nanoid(),
-                    completed : false,
-                    title,
-                }
-            }
-        }
+export const addToDoAsync = createAsyncThunk('todos/addToDoAsync', async (data) => {
+    const res = await axios.post(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`, data)
+    return res.data;
+})
 
-        },
-        toggle : (state,action) =>{
-            const {id} = action.payload;
+export const todosSlice = createSlice({
+    name: 'todos',
+    initialState: {
+        items: [],
+        İsLoading: false,
+        error: null,
+        activeFilter: 'all',
+        addNewTodoLoading: false,
+        addNewTodoError : null
+    },
+    reducers: {
+
+        toggle: (state, action) => {
+            const { id } = action.payload;
 
             const item = state.items.find(item => item.id === id)
             item.completed = !item.completed
         },
-        destroy : (state,action) =>{
+        destroy: (state, action) => {
             const id = action.payload;
-            const filtered = state.items.filter((item)=> item.id !== id)
+            const filtered = state.items.filter((item) => item.id !== id)
             state.items = filtered;
         },
-        changeActiveFilter : (state,action) =>{
+        changeActiveFilter: (state, action) => {
             state.activeFilter = action.payload;
         },
-        clearCompleted : (state) =>{
+        clearCompleted: (state) => {
             const filtered = state.items.filter(item => item.completed === false)
             state.items = filtered
         }
+        // add todo
+
 
 
     },
-    extraReducers : {
-        [getTodosAsync.pending] : (state,action) => {
+    extraReducers: {
+        [getTodosAsync.pending]: (state, action) => {
             state.İsLoading = true;
         },
-        [getTodosAsync.fulfilled] : (state,action) => {
-        state.items = action.payload;
-        state.İsLoading = false;
+        [getTodosAsync.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.İsLoading = false;
         },
-        [getTodosAsync.rejected] : (state,action) => {
+        [getTodosAsync.rejected]: (state, action) => {
             state.İsLoading = false;
             state.error = action.error.message;
-        }
+        },
+        [addToDoAsync.pending]: (state, action) => {
+            state.addNewTodoLoading = true
+
+        },
+        [addToDoAsync.fulfilled]: (state, action) => {
+            state.items.push(action.payload)
+            state.addNewTodoLoading = false
+        },
+        [addToDoAsync.rejected]: (state, action) => {
+            state.addNewTodoError = false;
+            state.error = action.error.message;
+        },
+
     }
 });
 
-/* Global Stateimizin items kısmına push metodu ile dizinin sonuna yeni değerler eklemek için kullanılır. İşlem sonucunda ise, dizinin yeni uzunluğunu geriye döner. */ 
+/* Global Stateimizin items kısmına push metodu ile dizinin sonuna yeni değerler eklemek için kullanılır. İşlem sonucunda ise, dizinin yeni uzunluğunu geriye döner. */
 /* Action. payload kısmı ise componentsde duran input formundan gelecektir. */
 
 export const selectTodos = (state) => state.todos.items;
@@ -78,9 +86,9 @@ export const selectFilteredTodos = (state) => {
         return state.todos.items;
     } // eğer todos all ise bütün global stateyi dön.
     return state.todos.items.filter((todo) =>
-    state.todos.activeFilter === 'active' ? todo.completed === false : todo.completed === true)
+        state.todos.activeFilter === 'active' ? todo.completed === false : todo.completed === true)
 };
 
-export const {addToDo, toggle, destroy, changeActiveFilter, clearCompleted} = todosSlice.actions;
+export const { toggle, destroy, changeActiveFilter, clearCompleted } = todosSlice.actions;
 
 export default todosSlice.reducer;
